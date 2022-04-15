949bdc42c6b312e07e9490b4559782a50cbb2a5c
Steven Edwards <stevenaedwards97@gmail.com>
2022-04-14 22:48:13 -0400

    Create jack.js

---
+++ commands/jack.js
@@ -0,0 +1,11 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('jack')
+		.setDescription('remove jack roles'),
+	async execute(interaction) {
+        const target = interaction.options.getMember('259082011539406848');
+        target.roles.remove("774365462984523806");
+	},
+};
